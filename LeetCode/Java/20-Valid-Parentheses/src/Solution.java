1ab893c6879a3e76d642c85573515bbd4c71acd3
KiyonamiYu <514723273@qq.com>
2019-04-05 10:26:56 +0800

    20-Valid-Parentheses completed!

---
+++ LeetCode/Java/20-Valid-Parentheses/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+/**
+ * @author KiyonamiYu
+ * @date 2019/4/5 9:53
+ */
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i ++) {
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                char stackTop = stack.pop();
+                if(ch == ')' && stackTop != '('
+                || ch == ']' && stackTop != '['
+                || ch == '}' && stackTop != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
