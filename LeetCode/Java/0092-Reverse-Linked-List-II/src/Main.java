e99150edaae5cd78738afae19c046a00eabc0836
KiyonamiYu <514723273@qq.com>
2019-04-17 12:16:55 +0800

    0092-Reverse-Linked-List-II completed!

---
+++ LeetCode/Java/0092-Reverse-Linked-List-II/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ListNode node1= new ListNode(1);
+        ListNode node2= new ListNode(2);
+        ListNode node3= new ListNode(3);
+        ListNode node4= new ListNode(4);
+        ListNode node5= new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = null;
+
+        new Solution().reverseBetween(node1, 2, 4);
+    }
+}
---
+++ LeetCode/Java/0092-Reverse-Linked-List-II/src/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * @author KiyonamiYu
+ * @date 2019/4/17 11:24
+ */
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+
+        ListNode opPre = null, ed = null;
+        ListNode prev = null, cur = dummyHead;
+
+        int index = 0;
+        while(index <= n) {
+            if(index == m - 1) {
+                //找到需反转段开始的前一个节点opPrev和结束节点ed
+                opPre = cur;
+                ed = cur.next;
+            }
+            if(index < m) {
+                cur = cur.next;
+            } else {
+                ListNode next = cur.next;
+
+                cur.next = prev;
+
+                prev = cur;
+                cur = next;
+            }
+            index ++;
+        }
+        opPre.next = prev;
+        ed.next = cur;
+        return dummyHead.next;
+    }
+}
