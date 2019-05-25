08d3e38246de3ed7113d85e8ef4fff5321c70c4c
KiyonamiYu <514723273@qq.com>
2019-05-25 23:46:26 +0800

    a little fix

--- LeetCode/JavaScript/test/0557-Reverse-Words-in-a-String-III.test.js
+++ LeetCode/JavaScript/test/0557-Reverse-Words-in-a-String-III.test.js
@@ -1,4 +1,4 @@
-import functions from '../src/557-Reverse-Words-in-a-String-III';
+import functions from '../src/0557-Reverse-Words-in-a-String-III';
 
 test('reverseWords: Let\'s take LeetCode contest',  () => {
     expect(functions('Let\'s take LeetCode contest')).toBe('s\'teL ekat edoCteeL tsetnoc');
